f200d16b00ecfe71218dcfaf6b40b5e3b45615fc
Dominic Payer <payerdom@my.uri.edu>
2022-08-27 17:42:11 -0400

    m

--- commerce/settings.py
+++ commerce/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nw_db',
+        'USER': 'admin',
+        'PASSWORD': '1221',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
