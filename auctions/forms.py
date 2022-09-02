6be0ff2163670afb73787c667d1a5f9094fd6313
Dominic Payer <payerdom@my.uri.edu>
2022-09-02 13:28:32 -0400

    Add forms.py, default SQL id field BigAutoField

---
+++ auctions/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm, HiddenInput
+from .models import *
+
+class NewListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['item', 'description', 'starting_bid', 'category', 'img', 'seller']
+        widgets = {'seller': HiddenInput()}
+
+class NewBidForm(ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['bid']
+
+class NewCommentForm(ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+class NewWatchForm(ModelForm):
+    class Meta:
+        model = Watchlist
+        fields = ['user', 'listing']
--- auctions/views.py
+++ auctions/views.py
@@ -5,31 +5,9 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.forms import ModelForm, HiddenInput
 
 from .models import *
-
-class NewListingForm(ModelForm):
-    class Meta:
-        model = Listing
-        fields = ['item', 'description', 'starting_bid', 'category', 'img', 'seller']
-        widgets = {'seller': HiddenInput()}
-
-class NewBidForm(ModelForm):
-    class Meta:
-        model = Bid
-        fields = ['bid']
-
-class NewCommentForm(ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ['comment']
-
-class NewWatchForm(ModelForm):
-    class Meta:
-        model = Watchlist
-        fields = ['user', 'listing']
+from .forms import *
 
 def check_if_watched(user, listing):
     if not user.watchlist.filter(listing = listing):
